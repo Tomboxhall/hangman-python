5e66ef93f33415341d4ae45a13dac8504f3f1d4c
Tom Boxhall <bomtoxhall@gmail.com>
2024-03-24 07:13:54 +0000

    Define the display_word function

--- hangman.py
+++ hangman.py
@@ -31,5 +31,15 @@ def choose_word(difficulty):
 
     return random.choice(words)
 
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    return display.strip()
+
 
 
