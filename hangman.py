5636691afc049503fe7a1c36c24e8489e03d40cd
Tom Boxhall <bomtoxhall@gmail.com>
2024-03-24 08:52:01 +0000

    Fix bug on line 5, incorrect characters

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import random
 import re
 
 def validate_word(word):
-    return bool(re.match("[^a-zA-Z]+$", word))
+    return bool(re.match("^[a-zA-Z]+$", word))
 
 def validate_difficulty(difficulty):
     return bool(re.match("^(easy|hard)$", difficulty))
