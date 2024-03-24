14d6da16495f18a31a67263f29e6972a68bad5b5
Tom Boxhall <bomtoxhall@gmail.com>
2024-03-24 07:06:05 +0000

    Write the docstring for the choose_word function

--- hangman.py
+++ hangman.py
@@ -7,3 +7,17 @@ def validate_word(word):
 def validate_difficulty(difficulty):
     return bool(re.match("^(easy|hard)$", difficulty))
 
+def choose_word(difficulty):
+    """
+    This function will choose a random word from the two word lists, based on the difficulty chosen.
+
+    Args:
+        difficulty (str): The difficulty level, either 'easy' or 'hard'.
+
+    Returns:
+        str: A randomly picked word from the word list files.
+
+    Raises:
+        ValueError: If the entry is not 'easy' or 'hard' the user will be prompted to retype.
+    """
+
